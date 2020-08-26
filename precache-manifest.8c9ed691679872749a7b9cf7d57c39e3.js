03519031353354e8c85e214d7b364a5075b8a9ee
Caroline Lester <carolineclester@gmail.com>
2020-08-25 22:37:39 -0400

    Updates

---
+++ precache-manifest.8c9ed691679872749a7b9cf7d57c39e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd2dbb69c315d09be42337d7959cd661",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "7a7c7b7b6577492f6968",
+    "url": "/clicky-game/static/css/main.43e492d8.chunk.css"
+  },
+  {
+    "revision": "15dcc2c7fb5639eed3be",
+    "url": "/clicky-game/static/js/2.1f6deb50.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.1f6deb50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a7c7b7b6577492f6968",
+    "url": "/clicky-game/static/js/main.2b67d88e.chunk.js"
+  },
+  {
+    "revision": "a2dd66fdceb11b57b463",
+    "url": "/clicky-game/static/js/runtime-main.3c5f3143.js"
+  },
+  {
+    "revision": "2190d4802b1361200885cd54a035c8b2",
+    "url": "/clicky-game/static/media/paper.2190d480.jpg"
+  }
+]);
